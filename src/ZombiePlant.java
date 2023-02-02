40ed7f11dd01450c95ef4e24d63fe53d5253573e
SypherPK <46838766+TenseUp@users.noreply.github.com>
2023-02-02 14:36:21 +0800

    Initial commit

--- src/ZombiePlant.java
+++ src/ZombiePlant.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class ZombiePlant {
+import org.junit.Test;
+public class ZombiePlant {
+    private int potency, treatments;
+
+    public ZombiePlant(int p, int t){
+        this.potency = p;
+        this.treatments = t;
+    }
+    public int treatmentsNeeded(){
+        return treatments;
+    }
+    public boolean isDangerous(){
+        if(treatments <= 1){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    public void treat(int c){
+        if (c > potency) {
+            treatments++;
+            return;
+        } else if ((treatments == 0) || (c <= 0)){
+            return;
+        }
+        treatments--;
+
+
+
+    }
+
+    public int getPotencyRequired() {
+        return potency;
+    }
 }
